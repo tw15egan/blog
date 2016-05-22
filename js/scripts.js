4c44d78f79414f65fc535704f1a1b9b5f2b618b1
TJ Egan <twegan@us.ibm.com>
2016-05-22 16:12:12 -0500

    more nav toggle fixes

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,22 @@ var navList = document.querySelector('#nav-content');
 
 navIcon.addEventListener('click', function(e) {
   e.preventDefault();
-  this.classList.toggle('active');
-  navList.classList.toggle('active');
+  if (navList.classList.contains('active')) {
+    this.classList.remove('active');
+    navList.classList.remove('active');
+    navList.classList.remove('toggle__content--scroll');
+    navList.classList.remove('toggle__content--mobile');
+  } else {
+    this.classList.add('active');
+    navList.classList.add('active');
+    if (window.scrollY > 80) {
+      navList.classList.add('toggle__content--scroll');
+    } else {
+      navList.classList.add('toggle__content--mobile');
+    }
+  }
+  
+  
 })
 
 var mainHeader = document.querySelector('#main-header');
@@ -23,6 +37,10 @@ window.addEventListener('scroll', function(e) {
       stickyNav.classList.add('sticky-nav');
     } else {
       navList.classList.remove('.toggle__content--active');
+      navIcon.classList.remove('active');
+      navList.classList.remove('active');
+      navList.classList.remove('toggle__content--scroll');
+      navList.classList.remove('toggle__content--mobile');
       mainHeader.classList.remove('main-header--active');
       socialList.classList.remove('social-list--active');
       stickyNav.classList.remove('sticky-nav');
